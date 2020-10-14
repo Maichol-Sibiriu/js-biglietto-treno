1cc3980de983c90bf726ef3d9c04a545690b60cb
Maichol <msibiriu@gmail.com>
2020-10-14 17:28:06 +0200

    rivisitazione con if, else if, else

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 // inizio esercizio
-
 // km che deve percorrere l'utente
 var km = prompt("quanti km devi fare?");
 console.log(km);
@@ -8,37 +7,21 @@ console.log(km);
 var age = prompt("quanti anni hai?");
 console.log(age);
 
-// info utente
-var info = km + age;
-console.log(info);
-
 // costo biglietto per km
-var price = 0.21;
+var price = 0.21 * km;
 
-// calcolo costo del biglietto per i km da percorrere (50km)
-price *= 50;
-console.log(price.toFixed (2) );
+var finalPrice = 0;
 
-// prezzo finale senza sconto
-var formatPrice = price.toFixed (2) + "€";
-console.log(formatPrice);
+if (age < 18) {
+  var under = price * 0.2;
+  finalPrice = price - under;
+}
+else if (age >= 65) {
+  var over = price * 0.4;
+  finalPrice = price - over;
+}
+else {
+  finalPrice = price;
+}
 
-// sconto minorenni
-var under = price * 20 / 100;
-console.log(under.toFixed (2));
-
-// sconto over 65
-var over = price * 40 / 100;
-console.log(over.toFixed (2));
-
-// prezzo finale con sconto minorenni
-var priceUnder = price - under;
-console.log(priceUnder.toFixed (2));
-
-// prezzo finale con sconto over 65
-var priceOver = price - over;
-console.log(priceOver.toFixed (2));
-
-// prezzo biglietto
-document.getElementById('number').innerHTML = "prezzo biglietto minorenni: " + priceUnder.toFixed (2) + "€";
-document.getElementById('number2').innerHTML = "prezzo biglietto over65: " + priceOver.toFixed (2) + "€";
+document.getElementById('number').innerHTML = "prezzo biglietto: " + finalPrice.toFixed(2) + "€";
